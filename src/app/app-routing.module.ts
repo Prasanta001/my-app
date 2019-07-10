6bf2228e4f7896576bc63d246a34437635b6fce4
Prasanta001 <prasantadutta976@gmail.com>
2019-07-10 15:05:05 +0530

    com2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+
 
 
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes), MatButtonModule, MatCheckboxModule],
+  exports: [RouterModule, BrowserAnimationsModule, MatButtonModule, MatCheckboxModule]
 })
 export class AppRoutingModule { }
+
